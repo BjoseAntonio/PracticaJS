cf735b4d3e15a7c6c42a33e1a0af5d552aec9453
BjoseAntonio <bas.9jose@gmail.com>
2023-05-08 07:28:49 -0300

    Asignacion de arc y prac

---
+++ 20.js
@@ -0,0 +1,7 @@
+// Diferencia entre metodo y funcion 
+const numero1 = 20;
+const numero2 = '20';
+
+console.log(parseInt(numero2)); //funcion 
+console.log(numero2.toString());//metodo
+// metodo siempre usa la variable Primero y funcion dentro de los parentes
---
+++ 21.js
@@ -0,0 +1,12 @@
+
+
+// 1- Declaracion de funcion 
+function sumar(){
+    console.log(10+10);
+}
+sumar();
+
+//2- Expresion de la funcion 
+const sumar2 = function(){
+    console.log(2+2);
+}

53cc0304dd22bc0d173f35708b0cf026247b6f6f
BjoseAntonio <bas.99jose@gmail.com>
2023-05-11 18:55:13 -0500

    28-For Loop

--- 28.js
+++ 28.js
@@ -1,1 +1,23 @@
 // for loops (iteradores) 
+//  for(let i = 1; i < 10; i++){
+//     if(i % 2 === 0){
+//         console.log(`El numero ${i} es PAR`);
+//     }else{
+//         console.log(`El numero ${i} es impar`);
+//     }
+//  }
+
+//While loop
+const carrito = [
+    {nombre: "Monitor 20 pulgadas", precio:300},
+    {nombre: "Television", precio: 500},
+    {nombre: "Tablet", precio: 1000},
+    {nombre: "Celular", precio: 350},
+    {nombre: "Mac", precio: 450}
+];
+
+for(let e = 0; e < carrito.length; e++){
+    console.long(e);
+}
+
+// Do while loop 

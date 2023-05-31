c967aa43bc5ee743acb1d356340e98baf8bae595
BjoseAntonio <jose.99basilio@gmail.com>
2023-05-31 07:31:03 +0200

    String

--- 04.js
+++ 04.js
@@ -1,6 +1,7 @@
 // String o cadenas de texto
 const producto =  'monito 20 pulgadas';
 //constructor 
+//
 const producto2 = String('Monitor 30 pulgadas');
 const producto3 = new String('Monitor 30 pulgadas');
            

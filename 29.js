2068413b59c5969355f33948b7b074400b74b0bf
BjoseAntonio <jose.99basilio@gmail.com>
2023-05-31 05:13:17 +0200

    forEach and map

--- 29.js
+++ 29.js
@@ -6,3 +6,18 @@ const carrito = [
     {nombre: "Celular", precio: 350},
     {nombre: "Mac", precio: 450}
 ];
+
+// Foreach 
+carrito.forEach(function(producto){
+    console.log(producto.nombre)
+})
+
+// map
+carrito.map(function(producto){
+    console.log(producto.nombre)
+})
+
+
+// Diferencia 
+// mostar cosas en pantalla o consola utilizar forEach : iterar y mostrar resultados
+// si se crea un nuevo arreglo utilizar map : Poder crear arreglos

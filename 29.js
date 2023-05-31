6439addd79b07da4f56aa2c781b84a54c5ecf11c
BjoseAntonio <jose.99basilio@gmail.com>
2023-05-31 03:47:48 +0200

    ForEach y Map

---
+++ 29.js
@@ -0,0 +1,8 @@
+// ForEach y map
+const carrito = [
+    {nombre: "Monitor 20 pulgadas", precio:300},
+    {nombre: "Television", precio: 500},
+    {nombre: "Tablet", precio: 1000},
+    {nombre: "Celular", precio: 350},
+    {nombre: "Mac", precio: 450}
+];

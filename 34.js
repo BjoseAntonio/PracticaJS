a62dd490f52b093f332f094af0871dfb64d9ac46
BjoseAntonio <bas.99jose@gmail.com>
2023-06-21 00:27:15 -0400

    Herencia

--- 34.js
+++ 34.js
@@ -15,19 +15,31 @@ class Producto {
 const producto2 = new Producto('Monitor 22"', 8000);
 const producto3 = new Producto('Monitor 32"', 10000);
 
-class Libro{
+// Herencia
+class Libro extends Producto{
     constructor(nombre, precio, isbn){
-        this.nombre = nombre;
-        this.precio = precio;
+        super(nombre, precio);
         this.isbn = isbn;
     }
+
+    formatearProducto(){
+        return ` ${super.formatearProducto() } y su ISBN es ${this.isbn}`
+    }
 }
+
+// class Libro{
+//     constructor(nombre, precio, isbn){
+//         this.nombre = nombre;
+//         this.precio = precio;
+//         this.isbn = isbn;
+//     }
+// }
 
 const libro = new Libro('Java', 589, '10921989318298183');
 
 console.log(producto2);
 console.log(producto3);
-console.log(libro);
+console.log(libro.formatearProducto());
 
 
 

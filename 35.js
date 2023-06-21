d1049defb5099ecedf0be52bb15b85562a871efb
BjoseAntonio <bas.99jose@gmail.com>
2023-06-21 01:21:23 -0400

    Trycacht

---
+++ 35.js
@@ -0,0 +1,13 @@
+// Trycatch 
+
+const numero1 = 20;
+const numero3 = 30;
+
+console.log(numero1);
+// console.log(numero2);  //Encuentra error y deja de ejecutar 
+try{
+    console.log(numero2);
+}catch(error){
+    console.log(error);
+}
+console.log(numero3);

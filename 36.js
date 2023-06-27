214c579717cdad1d300f53327d09d456618a9d50
BjoseAntonio <bas.99jose@gmail.com>
2023-06-26 23:33:10 -0400

    Promises JS

--- 36.js
+++ 36.js
@@ -1,11 +1,31 @@
 // Promises JS 
 const usuarioAutenticado = new Promise ( (resolve, reject) => {
-    const auth = true;
+    const auth =false; //autenticacion
 
     if(auth){
-        resolve();
+        resolve('Usuario autenticado');//Resultado
     }else{
-        
+        reject('Usuario no autenticado');//Error
     }
 
-})
+});
+
+// Utilizacion para mostrarlo y poderlo utilizar mensaje visual en html 
+usuarioAutenticado
+// .then(function(resultado){
+//     console.log(resultado);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+
+// Es similar que con el fuction pero con codigo mas reducido
+.then( resultado => console.log(resultado))
+.catch( error => console.log(error))
+
+// console.log(usuarioAutenticado);
+
+// En los Promises exiten 3 valores
+// Pending: No se ha cumplido pero tampoco se ha rechazado
+// Fulfilled: Ya se cumplio  
+// Rejected: Se ha rechazado o no se pudo cumplir

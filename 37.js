07d8580b07c35b37c9ae515bc5c053753eafa4fd
BjoseAntonio <bas.99jose@gmail.com>
2023-06-27 19:08:32 -0400

    Notification API

--- 37.js
+++ 37.js
@@ -1,6 +1,15 @@
 // Notification API 
 // querySelector ; Nos ayuda a seleccionar contenido html 
 const boton = document.querySelector('#boton');
-boton.addEventListener('click', function(){
-    console.log('Diste click');
+
+boton.addEventListener('click', () => {
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es ${resultado}`))
 });
+
+// Se muestra la notificacion cuando el usuario autoriza pernisos
+if(Notification.permission == 'granted'){
+    new Notification('Esta es una notificacion', {
+        icon: 'img/ccj.png'
+    })
+}

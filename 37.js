3033627a123e84c3739ec5f9427102a92ff7334f
BjoseAntonio <bas.99jose@gmail.com>
2023-06-27 18:04:55 -0400

    Notification API

---
+++ 37.js
@@ -0,0 +1,6 @@
+// Notification API 
+// querySelector ; Nos ayuda a seleccionar contenido html 
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', function(){
+    console.log('Diste click');
+});
